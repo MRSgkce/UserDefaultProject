8974d1be3ccdb275ab1e0b965501c28da9d7c864
MRSgkce <mursidegokce985@gmail.com>
2024-08-10 09:50:02 +0300

    Kaydet butonuyla not ekleme ve zamanı belirterek kaydetme özelliği eklendi. Ekran ilk açıldığında daha önce kayıt edilen not ve zaman gözüküyor.

--- UserDefault/ViewController.swift
+++ UserDefault/ViewController.swift
@@ -8,12 +8,42 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var zamanLabel: UILabel!
+    @IBOutlet weak var NotLabel: UILabel!
+    @IBOutlet weak var zamanTextField: UITextField!
+    @IBOutlet weak var NotTextField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // UserDefaults'tan verileri al
+                if let kaydedilenNot = UserDefaults.standard.string(forKey: "not") {
+                    NotLabel.text = "yapılacak iş: \(kaydedilenNot)"
+                }
+                
+                if let kaydedilenZaman = UserDefaults.standard.string(forKey: "zaman") {
+                    zamanLabel.text = "yapılacak zaman: \(kaydedilenZaman)"
+                }
     }
-
-
+    
+    @IBAction func kaydet(_ sender: Any) {
+        // UITextField'in metin özelliklerini UserDefaults'a kaydet
+                if let notText = NotTextField.text {
+                    UserDefaults.standard.setValue(notText, forKey: "not")
+                }
+                
+                if let zamanText = zamanTextField.text {
+                    UserDefaults.standard.setValue(zamanText, forKey: "zaman")
+                }
+                
+                // Label'ları güncelle
+                NotLabel.text = "yapılacak iş: \(NotTextField.text ?? "")"
+                zamanLabel.text = "yapılacak zaman: \(zamanTextField.text ?? "")"
+            }
+        
+    
+    
+    @IBAction func sil(_ sender: Any) {
+    }
+    
 }
 
